2024eb348641522bc39c0bb0930f27e12df6666a
Administrator <245561237@qq.com>
2017-12-19 14:23:41 +0800

    改图片api

--- repository/article.go
+++ repository/article.go
@@ -133,7 +133,7 @@ func GetArticleCursorByRank(rank float64, limit int, tags []int64) (rows []Artic
 
 // ArticleAfter 修改文章
 func ArticleAfter(article *orm.Article) {
-	article.Cover = "https://p2hr.com/" + base64.URLEncoding.EncodeToString([]byte(article.Cover))
+	article.Cover = "https://img.readfollow.com/" + base64.URLEncoding.EncodeToString([]byte(article.Cover))
 	article.URL = strings.Replace(article.URL, `http://`, `https://`, -1)
 	article.URL = strings.Replace(article.URL, `#rd`, "&scene=27#wechat_redirect", 1)
 
